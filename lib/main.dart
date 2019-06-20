268bf7c1a0e7ad6aabe5d485c74bb1db351346f2
Joseph Saw <joesawtw@gmail.com>
2019-06-20 22:45:12 +0800

    P3: monitoring the progress of the animation

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,29 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   void initState() {
     super.initState();
     controller =
-        AnimationController(duration: const Duration(seconds: 5), vsync: this)..repeat();
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
 
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)..addStatusListener((AnimationStatus status) {
+      print('$status');
+      if(status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
 
-      controller.forward();
+    controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation,);
+  Widget build(BuildContext context) => AnimatedLogo(
+        animation: animation,
+      );
 }
 
-
 class AnimatedLogo extends AnimatedWidget {
-  AnimatedLogo({Key key, Animation<double> animation}) : super(key: key, listenable: animation);
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
 
   @override
   Widget build(BuildContext context) {
